d66d086391f65f89712e80886f58d16c73c1bd0e
nastehoabdiaden10-ai <nastehoabdiaden10-ai@gmail.com>
2026-07-20 02:58:41 -0700

    updated practice.py file

--- practice.py
+++ practice.py
@@ -1,3 +1,4 @@
+#Names and age:
 name=nasteho
 age=22
 print("name")
@@ -6,13 +7,13 @@ if age >=18:
     print("wxaa tahay qaangaar")
 else:
     print("ma tihid qaangaar")
-
+# numbers:
     num1=10
     num2=20
     num3=30
     sum=nu1+num2+num3
     print("sum")
-
+# numbers:
     n=80
     m=10
     sum=n+m
